7f6d20c885f648f045e5ccfc431be9bc1cb17d0b
Kwarto <dbitsolu@gmail.com>
2023-01-15 12:46:19 +0000

    Added Navlist

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import {NavBarWrapper} from './NavBarElement'
 const NavBar = () => {
   return (
-    <NavBarWrapper>
-
+    <>
+      <NavBarWrapper>
+      <p>NavBar</p>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </NavBarWrapper>
+    </>
   )
 }
 
