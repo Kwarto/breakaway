f1f0bfeb187aa5abc4f2628fb6faed164009cc2e
Kwarto <dbitsolu@gmail.com>
2023-01-16 02:39:11 +0000

    Added hero items

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { HeroWrapper } from './HeroElement'
-import {Category} from '../../sections'
+import { HeroWrapper, HeroContent, HeroSlide, HeroRight, ImgWrap } from './HeroElement'
+import { Category } from '../../sections'
+import banner1 from '../../img/huge.png';
+import bloc1 from '../../img/bloc1.jpg';
 const Hero = () => {
   return (
     <HeroWrapper>
       <Category />
+      <HeroContent>
+        <HeroSlide>
+          <img src={banner1} alt="" />
+        </HeroSlide>
+        <HeroRight>
+          <ImgWrap>
+            <div>
+              <img src={bloc1} alt="" />
+            </div>
+          </ImgWrap>
+          <ImgWrap>
+            <div>
+            <img src={bloc1} alt="" />
+           </div>
+          </ImgWrap>
+        </HeroRight>
+      </HeroContent>
     </HeroWrapper>
   )
 }
--- src/sections/Hero/HeroElement.jsx
+++ src/sections/Hero/HeroElement.jsx
@@ -1,5 +1,39 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HeroWrapper = styled.div`
-  
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+export const HeroContent = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 65% 33%;
+  gap: 2%;
+`;
+
+export const HeroSlide = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+export const HeroRight = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  gap: 10px;
+`;
+
+export const ImgWrap = styled.div`
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  div {
+    width: 52%;
+    img {
+      width: 100%;
+      border-radius: 10px;
+    }
+  }
+`;
